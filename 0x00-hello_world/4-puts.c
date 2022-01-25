f2d41ae8dfcc4603a99e30d9c5994252a9a5fe81
premiereelementllc <premiereelementllc@gmail.com>
2022-01-24 18:37:06 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* writing our first compilable program using C */
 
-int main (void)
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
