ec4081b441202606c9adf22fce2419c203cd21c4
premiereelementllc <premiereelementllc@gmail.com>
2022-01-24 21:51:00 -0800

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+/**
+ * main -starting program
+(*
+ *Description: prints the phrase and a new line)?
+ *Return: return (0) required function
+ */
+
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
+}
