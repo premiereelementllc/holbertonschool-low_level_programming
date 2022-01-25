70f9d905f4bc0f1645cc4e90e95f498f5c596aa2
premiereelementllc <91158034+premiereelementllc@users.noreply.github.com>
2022-01-24 19:48:27 -0800

    Update 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main -starting program
+(*
+ *Description: prints the phrase and a new line)?
+ *Return: return (0) required function
+ */
+
+#include <stdio.h>
+int main(void) 
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n")
+    return (0);
+}
