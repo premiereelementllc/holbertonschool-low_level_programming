2fd0884f9f49b9caec1981ff03cbd70ece69f0d3
root <premiereelementllc@gmail.com>
2022-01-25 21:31:58 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+/**
+ * main -starting program
+(*
+ *Description: prints the phrase and a new line)?
+ *Return: return (0) required function
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+
+{
+	char x;
+	int j;
+
+	for (j = 0; j < 10; j++)
+		putchar (j % 10 + '0');
+	for (x = 'a'; a <= 'f'; x++)
+		putchar (x);
+	putchar('\n');
+	return (0);
+}
+
