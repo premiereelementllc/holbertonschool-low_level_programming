b09cbe93ccd792d5f65e5c10930d077d8c4e3961
root <premiereelementllc@gmail.com>
2022-01-26 21:39:52 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet
+ * Return: 0
+ */
+
+int main(void)
+
+{
+void print_alphabet(void)
+{
+	char(a2z);
+
+	for (a2z = 'a'; a2z <= 'z'; ++a2z)
+	{
+		_putchar(a2z);
+	}
+
+	_putchar('\n');
+}
+}
