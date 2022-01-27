ee5e04bc96e7ae08ca0b8e7378deed3596d73e20
root <premiereelementllc@gmail.com>
2022-01-26 18:27:09 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main -starting program
+(*
+ *Description: prints the phrase and a new line)?
+ *Return: return (0) required function
+ */
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
