8117f19fd643fc7d7e11c3b18e2747cfcb5a605d
root <premiereelementllc@gmail.com>
2022-01-28 20:17:02 -0800

    q

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add- adding two integers
+ * @a: first integer added
+ * @b: second integer added
+ * Return: integer
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main check the code
+ *
+ * Retur: Always 0
+ */
+
+int main(void)
+
+{
+	int add(int, int);
+        return (0);
+}
