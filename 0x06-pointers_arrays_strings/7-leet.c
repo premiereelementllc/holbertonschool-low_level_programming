96c1d8ab200fdc29736c8e3d88f3a7bf2fe3277c
root <premiereelementllc@gmail.com>
2022-02-12 20:09:45 -0800

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *leet- function that encodes a string of leters into numbers
+ * @input: array of letters
+ * Return: output
+ */
+
+char *leet(char *input)
+{
+	char *output = input;
+	char *letter = "ltoea";
+	char *num = "17034";
+	int i;
+
+	while (*input)
+	{
+		for (i = 0; letter[i]; i++)
+		{
+			*input = num[i];
+		}
+		input++;
+	}
+	return (output);
+}
