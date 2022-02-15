69d6a2996bcad4ef00016bcb4ea24a9801b1fd38
premiereelementllc <91158034+premiereelementllc@users.noreply.github.com>
2022-02-14 16:21:58 -0800

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strcat- concatenate two strings.
+ * @dest: concatenation
+ * @src: string to concatenate
+ * Return: string after concatenation
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int b = 0, i;
+
+	while (dest[b])
+		b++;
+
+	for (i = 0; src[i]; i++, b++)
+		dest[b] = src[i];
+	dest[b] = 0;
+	return (dest);
+}
