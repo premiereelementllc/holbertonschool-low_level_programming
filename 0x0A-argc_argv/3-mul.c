b6bcf924285296f2bf1a10bf299d908360a23984
root <premiereelementllc@gmail.com>
2022-02-15 18:49:28 -0800

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv)
 	if (argc < 3)
 	{
 		printf("Error\n");
-		return(EXIT_FAILURE);
+		return (EXIT_FAILURE);
 	}
 
 	printf("%d\n", atoi(*(argv + 1)) * atoi(*(argv + 2)));
