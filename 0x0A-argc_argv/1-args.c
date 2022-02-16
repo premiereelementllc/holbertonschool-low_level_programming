9c49043c9efc8b2450b11c09641d8d08e86ffed8
root <premiereelementllc@gmail.com>
2022-02-15 18:03:01 -0800

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,13 +4,11 @@
 /**
  * main- print number of arguments
  * @argc: count arguments from command line
- * @argv: arguments unused
  * Return: (1)
  */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc)
 {
-	argv = NULL;
 	printf("%d\n", argc);
 	return (1);
 }
