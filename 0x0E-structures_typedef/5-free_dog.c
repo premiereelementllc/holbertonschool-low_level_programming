5a36168d1d90379bec158626ca4bcac3deb33880
root <premiereelementllc@gmail.com>
2022-02-22 21:25:54 -0800

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog,j"
+#include <stdlib.h>
+
+ /**
+ * free_dog- free the dogs
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
