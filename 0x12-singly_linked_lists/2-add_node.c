23b69c65b922f87e3626864db6a736a82994429d
root <premiereelementllc@gmail.com>
2022-03-14 17:13:02 -0700

    2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node- this function attach node in a linked list
+ * @head: start of linked list
+ * @str: string to store in new node
+ * Return: new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	int len;
+
+	node = malloc(sizeof(list_t));
+
+	if (!node)
+		return (NULL);
+
+	node->str = strdup(str);
+
+	for (len = 0; str[len]; len++)
+	{}
+
+	node->len = len;
+
+	if (!*head)
+		*head = node;
+	else
+	{
+		node->next = *head;
+		*head = node;
+	}
+	return (node);
+}
