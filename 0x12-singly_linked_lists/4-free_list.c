86446c24ba5871953ecfcc6d006fe0e45f273602
root <premiereelementllc@gmail.com>
2022-03-14 18:46:47 -0700

    4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * free_list- free all malloc previous
+ * @head: linked list
+ */
+
+void free_list(list_t *head)
+{
+	if (!head)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
