256359692664d1d2791efb47ecda9d19ae3e614a
root <premiereelementllc@gmail.com>
2022-04-04 21:47:06 -0700

    2

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -7,3 +7,19 @@
  * Return: address of new node or NULL is fail
  */
 
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (*head)
+		(*head)->prev = new;
+	*head = new;
+
+	return (new);
+}
