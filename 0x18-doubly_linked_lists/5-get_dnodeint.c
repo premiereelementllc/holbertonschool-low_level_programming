112c74e2d2b0e66e8a419374665337971a0f3d7f
root <premiereelementllc@gmail.com>
2022-04-04 21:58:57 -0700

    qs

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -1,30 +1,25 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node of a list
- * @head: head node of list
- * @index: index of nodes starting at 0
- * Return: node or NULL if fail
+ * get_dnodeint_at_index - finds a specific node of a linked list
+ * @head: pointer to the beginning of the list
+ * @index: index of the node to retrieve
+ *
+ * Return: pointer to the indexed node, or NULL on failure
  */
-
-dlistint_t *getdnodeint_at_index(dlistint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	unsigned int count = 0;
-	dlistint_t *curr = head;
+	unsigned int i;
 
-	if (!head)
+	if (head == NULL)
 		return (NULL);
 	if (index == 0)
 		return (head);
-
-	while ((curr != NULL) && (count < index))
+	for (i = 0; i < index; i++)
 	{
-		curr = curr->next;
-		count++;
-
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
 	}
-	if ((count < index) || (curr == NULL))
-		return (NULL);
-	else
-		return (curr);
+	return (head);
 }
